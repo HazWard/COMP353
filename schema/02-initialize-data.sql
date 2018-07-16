d688af26db8ebf90c88acb1fa7ab22f516417595
Rea <reasharma091@hotmail.com>
2018-07-16 18:22:11 -0400

    updating the insert statements to get code to compile without error

--- schema/02-initialize-data.sql
+++ schema/02-initialize-data.sql
@@ -1,5 +1,6 @@
 USE company_cms;
 
+SET FOREIGN_KEY_CHECKS=0;
 # Departments
 INSERT INTO departments(department_id, department_name) VALUES (1,'Development');
 INSERT INTO departments(department_id, department_name) VALUES (2,'QA');
@@ -9,11 +10,11 @@ INSERT INTO departments(department_id, department_name) VALUES (5,'Business Inte
 INSERT INTO departments(department_id, department_name) VALUES (6,'Networking');
 
 # Contracts
-INSERT INTO contracts(contract_id, contract_type, type_of_service, acv, initial_amount, manager_id, company_name) VALUES(0, 'Gold', 'On-premises', 3000, 1500.00, 0, 'Walmart');
-INSERT INTO contracts(contract_id, contract_type, type_of_service, acv, initial_amount, manager_id, company_name) VALUES(1, 'Silver', 'Cloud', 2000, 500.00, 1337, 'Wallgreens');
-INSERT INTO contracts(contract_id, contract_type, type_of_service, acv, initial_amount, manager_id, company_name) VALUES(2, 'Diamond', 'On-premises', 6500, 1000.00, 0, 'IKEA');
+INSERT INTO contracts(contract_id, contract_type, type_of_service, acv, initial_amount, manager_id, company_name) VALUES(0, 'Gold', 'On-premises', 3000, 1500.00, 1234, 'Walmart');
+INSERT INTO contracts(contract_id, contract_type, type_of_service, acv, initial_amount, manager_id, company_name) VALUES(1, 'Silver', 'Cloud', 20000, 500.00, 1337, 'Wallgreens');
+INSERT INTO contracts(contract_id, contract_type, type_of_service, acv, initial_amount, manager_id, company_name) VALUES(2, 'Diamond', 'On-premises', 6500, 1000.00, 1234, 'IKEA');
 INSERT INTO contracts(contract_id, contract_type, type_of_service, acv, initial_amount, manager_id, company_name) VALUES(3, 'Premium', 'Cloud', 10000, 2500.00, 1337, 'IKEA');
-INSERT INTO contracts(contract_id, contract_type, type_of_service, acv, initial_amount, manager_id, company_name) VALUES(4, 'Silver', 'On-premises', 750, 250.00, 0, 'Walmart');
+INSERT INTO contracts(contract_id, contract_type, type_of_service, acv, initial_amount, manager_id, company_name) VALUES(4, 'Silver', 'On-premises', 8500, 250.00, 1234, 'Walmart');
 
 # Companies
 INSERT INTO client_companies(company_name, company_email, rep_first_name, rep_last_name, rep_middle_initial, city, province) VALUES('Walmart', 'walmart@email.com', 'Simba', 'Lion','S', 'Quebec', 'QC');
@@ -29,13 +30,5 @@ INSERT INTO employees(employee_id, first_name, last_name, department, manager_id
 INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(226, 'Sam', 'Wood', 1, 1337);
 INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(227, 'Paul', 'Amsterdam', 2, 1337);
 INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(228, 'Paula', 'Bambam', 3, 1337);
-INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(229, 'Jocelyn', 'Cambridge', 4, 0);
-INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(230, 'Tom', 'Oooma', 5, 0);
-INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(231, 'Unagi', 'Sushi', 5, 0);
-INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(232, 'Paul', 'Amsterdam', 4, 0);
-INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(233, 'Boston', 'Tourists', 3, 0);
-INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(234, 'Random', 'Namehere', 2, 0);
-INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(235, 'Meme', 'Gene', 1, 0);
-INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(236, 'Halo', 'Beyonce', 0, 0);
-INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(237, 'Mcdo', 'Nouggats', 1, 0);
-INSERT INTO employees(employee_id, first_name, last_name, department, manager_id) VALUES(238, 'Dave', 'Biggie', 2, 0);
+
+
