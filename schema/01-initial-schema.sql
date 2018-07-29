a9ee197a63acb99485b4d233ed999d1869707151
Starly Solon <starlysolon@gmail.com>
2018-07-28 20:07:48 -0400

    Updated schema
    - Added data about provinces
    - Added a few fields
    	- Satisfactory score (for contracts)
    	- Insurance plans (for employees)
    	- Reimbursement percentage values (for insurance plans)

--- schema/01-initial-schema.sql
+++ schema/01-initial-schema.sql
@@ -1,17 +1,29 @@
-# Initialize database
+# Init
 DROP DATABASE company_cms;
 CREATE DATABASE company_cms;
 USE company_cms;
 
 # Initialize tables
+CREATE TABLE provinces (
+    prov_abbrev CHAR(2) PRIMARY KEY,
+    prov_name VARCHAR(255),
+);
+
+CREATE TABLE cities (
+    city_id INT PRIMARY KEY,
+    city_name VARCHAR(255),
+    province CHAR(2),
+    FOREIGN KEY (province) REFERENCES provinces(abbrev)
+);
+
 CREATE TABLE client_companies(
     company_name VARCHAR(255) PRIMARY KEY,
     company_email VARCHAR(255),
     rep_first_name VARCHAR(255),
     rep_last_name VARCHAR(255),
     rep_middle_initial VARCHAR(255),
-    city VARCHAR(255),
-    province VARCHAR(255)
+    city INT,
+    FOREIGN KEY (city) REFERENCES cities(city_id)
 );
 
 CREATE TABLE departments(
@@ -19,18 +31,26 @@ CREATE TABLE departments(
     department_name VARCHAR(255)
 );
 
+CREATE TABLE insurance_plans(
+    plan_id INT PRIMARY KEY,
+    plan_name VARCHAR(255),
+    reimbursement_perc FLOAT(4,2)
+);
+
 CREATE TABLE employees(
     employee_id INT PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    department INT,
-    FOREIGN KEY (department) REFERENCES departments(department_id),
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id),
     manager_id INT,
-    FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id),
+    insurance_plan INT,
+    FOREIGN KEY (insurance_plan) REFERENCES insurance_plans(plan_id)
 );
 
 CREATE TABLE contracts(
-    contract_id INT PRIMARY KEY AUTO_INCREMENT,
+    contract_id INT PRIMARY KEY,
     contract_type VARCHAR(255),
     type_of_service VARCHAR(255),
     acv FLOAT,
@@ -39,5 +59,6 @@ CREATE TABLE contracts(
     manager_id INT,
     FOREIGN KEY (manager_id) REFERENCES employees(employee_id),
     company_name VARCHAR(255),
-    FOREIGN KEY (company_name) REFERENCES client_companies(company_name)
+    FOREIGN KEY (company_name) REFERENCES client_companies(company_name),
+    satisfaction_score INT DEFAULT 5
 );
