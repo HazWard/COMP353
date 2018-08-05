22a1ebc4a9512309f68ec4fdaf5c56bdc750f93d
Ishmam Murtaza <ishmammurtaza@gmail.com>
2018-08-05 16:26:31 -0400

    Create client form completed (#19)
    * Insertion to clients table completed
    * Insertion to credential table
    * Front end form validation for create client
        - added head.php to be included on pages for navbar
        - added sales associate home page
    
    * Moved backend files to /api
    * Cities now populated from api
    * Changed backend to match new schema

--- site/api/core.php
+++ site/api/core.php
@@ -3,7 +3,7 @@ class Database{
 
     // specify your own database credentials
     private $host = "localhost";
-    private $db_name = "company-cms";
+    private $db_name = "company_cms";
     private $username = "root";
     private $password = "";
     public $conn;
