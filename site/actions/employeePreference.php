757932c972b4d34b514fe7cf9671a03d5cfdad16
Starly Solon <starlysolon@gmail.com>
2018-08-06 13:56:17 -0400

    Changes done

--- site/actions/employeePreference.php
+++ site/actions/employeePreference.php
@@ -1,12 +1,12 @@
 <?php
-
-if(isset($_SESSION['userName'])) {
-$id = 'userName'
+session_start();
+if(isset($_SESSION['username'])) {
+    $id = $_SESSION['username'];
 }
 
 $employeeArray = $_POST;
 
-$service_url = 'http://localhost:8888/COMP353/api/index.php/employees/$id/preferences';
+$service_url = 'http://localhost/api/index.php/employees/'.$id.'/preferences';
 $curl = curl_init($service_url);
 $curl_post_data = array(
     'category' => $employeeArray['category'],
@@ -18,11 +18,11 @@ curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
 curl_exec($curl);
 
 $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
 if ($httpcode == 200) {
 	//redirects to next page: log hours (will be created soon)
     header("Location: /COMP353/site/logHours.html");
-}
-else{
+} else{
     echo 'Error';
 }
 
