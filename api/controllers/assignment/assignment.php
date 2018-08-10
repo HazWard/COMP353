1ba22e21d9484f1378d3f9101b8a469f41717c40
Ishmam Murtaza <ishmammurtaza@gmail.com>
2018-08-09 22:09:15 -0400

    hours logged now works

--- api/controllers/assignment/assignment.php
+++ api/controllers/assignment/assignment.php
@@ -124,7 +124,7 @@ class AssignmentController
         
         $queryTxt = "UPDATE ".AssignmentController::$assigned_table_name." SET hours_worked = :numHours WHERE employee_id =:eid AND contract_id =:cid AND hours_worked is NULL";
         $stmt = $connection->prepare($queryTxt);
-        $stmt->bindValue(':numHours', $request_body['numHours'], PDO::PARAM_INT);
+        $stmt->bindValue(':numHours', $request_body['hours_worked'], PDO::PARAM_INT);
         $stmt->bindValue(':eid', $eid_param, PDO::PARAM_INT);
         $stmt->bindValue(':cid', $cid_param, PDO::PARAM_INT);
         $stmt->execute();
--- site/actions/employeeHours.php
+++ site/actions/employeeHours.php
@@ -8,6 +8,8 @@ if(isset($_SESSION['username'])) {
 $cid = $_POST["contractid"];
 
 $logHourArray = $_POST;
+
+
 $service_url = 'http://localhost/COMP353/api/index.php/employees/'.$eid.'/contracts/'.$cid;
 $curl = curl_init($service_url);
 $curl_post_data = array(
@@ -15,6 +17,8 @@ $curl_post_data = array(
     "contract_id"=> $cid,
     "hours_worked"=> $logHourArray['hours']
        );
+
+
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
