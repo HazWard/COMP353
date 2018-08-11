204b5131f56eff55f8f54360fe9eace65e6d5385
Rea <reasharma091@hotmail.com>
2018-08-10 22:49:36 -0400

    Create satScore.php

---
+++ site/satScore.php
@@ -0,0 +1,23 @@
+<?php
+
+$cid = $_POST['cid'];
+$score = $_POST['score'];
+
+
+$service_url = "http://localhost:8888/api/index.php/contracts/{$cid}/score";
+$curl = curl_init($service_url);
+$curl_post_data = array(
+    "cid" => $cid,
+    "score" => $score
+);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
+curl_exec($curl);
+$httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+if ($httpcode == 200) {
+    header("Location: /site/clientHome.php");
+} else{
+    echo 'Error';
+}
+?>
