f1943a1ca2d1746a3c6a2a0eea56be25d5ca6ded
Rea <reasharma091@hotmail.com>
2018-08-11 17:03:40 -0400

    Update employeePreference.php

--- site/actions/employeePreference.php
+++ site/actions/employeePreference.php
@@ -4,13 +4,16 @@ if(isset($_SESSION['username'])) {
     $id = $_SESSION['username'];
 }
 
+$eid = $_POST['eid'];
+$category = $_POST['category'];
+$type = $_POST['type'];
 $employeeArray = $_POST;
 
-$service_url = 'http://localhost/COMP353/api/index.php/employees/'.$id.'/preferences';
+$service_url = 'http://localhost:8888/api/index.php/employees/'.$eid.'/preferences';
 $curl = curl_init($service_url);
 $curl_post_data = array(
-    'category' => $employeeArray['category'],
-    'type' => $employeeArray['type']
+    'category' => $category,
+    'type' => $type
     );
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POST, true);
@@ -20,8 +23,9 @@ curl_exec($curl);
 $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
 if ($httpcode == 200) {
-    header("Location: /COMP353/site/employeeHome.php");
+    header("Location: /site/employeeHome.php");
 } else{
     echo 'Error';
 }
 ?>
+
